ad7bca396b1c2d31c42542b35e95a153fb9cbcbb
adarici <comdarici@gmail.com>
2021-10-26 23:08:44 +0200

    deneme

--- src/test/java/test_data/ReviewJsonPlaceHolderTestData.java
+++ src/test/java/test_data/ReviewJsonPlaceHolderTestData.java
@@ -7,8 +7,8 @@ public class ReviewJsonPlaceHolderTestData {
     public Map<String,Object> expectedDataSetUp(){
      Map<String,Object> expectedData = new HashMap<>();
      expectedData.put("userId",55);
-     expectedData.put("title","Tidy your room");
-     expectedData.put("completed",true);
+//     expectedData.put("title","Tidy your room");
+//     expectedData.put("completed",true);
         return expectedData;
     }
 }
